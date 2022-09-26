3c27d84280d281a5a56aafdd2362a335b830c790
Felipe Cavalcanti <112189993+felipecavalcanti5@users.noreply.github.com>
2022-09-26 18:01:56 -0300

    Update TED e exercícios (1 de 2).py

--- aula 07/TED e exercícios (1 de 2).py
+++ aula 07/TED e exercícios (1 de 2).py
@@ -1,5 +1,5 @@
-#As maçãs custam R$ 1,30 cada se forem compradas menos de uma dúzia, e R$ 1,00 se forem compradas
-#pelo menos 12. Escreva um programa que leia o número de maçãs compradas, calcule e escreva o custo total da compra.
+# As maçãs custam R$ 1,30 cada se forem compradas menos de uma dúzia, e R$ 1,00 se forem compradas pelo menos 12. 
+# Escreva um programa que leia o número de maçãs compradas, calcule e escreva o custo total da compra.
 
 preço1 = 1.30
 preço2 = 1.00
